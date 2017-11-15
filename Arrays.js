8348843f6f3f7e825ee3c335c4b776715b765555
Uma Mahesh <uma.kondapally@gmail.com>
2017-11-15 07:22:28 +0530

    Array.of method

---
+++ Arrays.js
@@ -0,0 +1,10 @@
+//1. Array.of(e1[,......],[en]);
+console.log('Array.of(7):= ', Array.of(7));//[7]
+console.log('Array.of()', Array.of());//[]
+console.log('Array.of(undefined) := ', Array.of(undefined));//[undefined]
+console.log('Array.of(7,6,5,4) := ', Array.of(7, 6, 5, 4));//[7,6,5,4]
+console.log('new Array(7) := ', new Array(7));//[,,,,,,,] means 7 empty items
+let arr = [1, 2, 3, 4, 5];
+console.log(Array.of(arr));//[ [ 1, 2, 3, 4, 5 ] ]
+console.log(Array.of(...arr));//[ 1, 2, 3, 4, 5 ]
+console.log(Array.of('uma mahesh'));//[ 1, 2, 3, 4, 5 ]
